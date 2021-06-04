ff54c470c5b51a312cc8bc66b190deddda019a52
Elisandra Cristina de Ramos Silva <elisandra.silva@etec.sp.gov.br>
2021-06-03 21:46:30 -0300

    Adiconado o componente Toast

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,8 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController) {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
@@ -24,12 +26,20 @@ export class Tab1Page {
         }, {
           text: 'Sim favoritar',
           handler: () => {
-            console.log('Confirm Okay');
+            this.apresentarToast();
           }
         }
       ]
     });
     await alert.present();
+  }
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Filme Adicionado aos favoritos',
+      duration: 2000,
+      color:'success'
+    });
+    toast.present();
   }
 
 }
